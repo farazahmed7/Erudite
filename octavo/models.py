2afa6eede9e9a1a536f52416396d60b985beed1c
faraz <faraz.dipsite@gmail.com>
2016-07-29 11:59:11 +0530

    added octavo

---
+++ octavo/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+# Create your models here.
+
+class Search(models.Model):
+	sem=models.TextField()
+	def __str__(self):
+		return self.sem
+	
+class Subject(models.Model):
+	sub=models.TextField()
+	def __str__(self):
+		return self.sub	
+	
+	
